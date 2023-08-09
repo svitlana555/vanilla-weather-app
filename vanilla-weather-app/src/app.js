35d21deda8f25616b2ae131409e4275121af7c49
svitlana555 <svetlanakuznetsova2013@gmail.com>
2023-08-09 23:16:04 +0300

    Update the time

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -1,5 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours} : ${minutes}`;
+}
 function displayTemperature(response) {
-  console.log(response.data);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
   let temperatureElement = document.querySelector("#temperature");
@@ -10,8 +31,10 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 let apiKey = "13ef0853foc2a0082c452bf43308286t";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query={New York}&key=${apiKey}&units=metric`;
-console.log(apiUrl);
+
 axios.get(apiUrl).then(displayTemperature);
